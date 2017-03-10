01d24484ed4106761712645d987c2fbbed212120
adgvcxz <344292600@qq.com>
2017-03-10 21:02:27 +0800

    初始化项目

---
+++ app/src/main/java/com/adgvcxz/example/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.adgvcxz.example
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * zhaowei
+ * Created by zhaowei on 2017/3/10.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ library/src/main/java/com/adgvcxz/adgplayer/AdgVideoPlayer.kt
@@ -0,0 +1,9 @@
+package com.adgvcxz.adgplayer
+
+/**
+ * zhaowei
+ * Created by zhaowei on 2017/3/10.
+ */
+class AdgVideoPlayer {
+
+}
